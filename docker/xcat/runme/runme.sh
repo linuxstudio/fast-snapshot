8e864ef9c5bc245afbc04100975f198de191d265
Bruno Pereira <brunopereira81@gmail.com>
2014-05-22 00:12:35 +0200

    fixed runme script to format 1 disk and create a etx4 fs in it

--- docker/xcat/runme/runme.sh
+++ docker/xcat/runme/runme.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+
 if [ -b /dev/sda ]; then
-   parted -s /dev/sda mklabel gpt
-   parted -s /dev/sda mkpartfs logical ext2 0G 100%
+   parted /dev/sda mklabel dos
+   sync
+   blockdev --rereadpt /dev/sda 2> /dev/null &
+   wait %1
+   parted -s --aling cylinder /dev/sda "mkpart logical ext2 0G 100%"
+   sleep 5
+   sync
+   blockdev --rereadpt /dev/sda 2> /dev/null &
+   wait %1
+   mkfs.etx4 /dev/sda1
 fi
-
-
